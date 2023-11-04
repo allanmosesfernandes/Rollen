3f88ca22717ad8f8d7d367482f8558e586f66c2f
Allan Fernandes <allan.fernandes@responsiblelife.co.uk>
2023-11-04 20:50:51 +0000

    Refactor components and update images

---
+++ src/components/utils/Buttons/WhiteButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhiteButton = () => {
+  return (
+    <button className='w-[200px] p-4 text-sm bg-transparent border-2 border-white font-semibold text-white rounded-md'>More</button>
+  )
+}
+
+export default WhiteButton
---
+++ src/components/utils/Buttons/YellowButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const YellowButton = () => {
+  return (
+    <button className='w-[200px] p-4 text-md bg-rol-yellow font-geistSemiBold text-black rounded-md'>Download CV</button>
+  )
+}
+
+export default YellowButton
